1bdc9f3806a1631e9924450562b1d2da8a62b9c7
Scott Forehand <stforehand@gmail.com>
2019-02-23 11:58:36 -0600

    Add files via upload

---
+++ Get-RandomPassword.ps1
@@ -0,0 +1,13 @@
+﻿# Random Password Generator
+
+$KeyLength = 12
+
+[Reflection.Assembly]::LoadWithPartialName("System.Web") | Out-Null
+Do {
+   $Password = [System.Web.Security.Membership]::GeneratePassword($KeyLength,[Math]::Floor(($KeyLength/2)))
+} Until ($Password -Match '\d')
+
+Set-Clipboard -Value $Password
+Write-Host ""
+Write-Host "The password has been copied to the clipboard."
+Write-Host $Password -ForegroundColor Gray
